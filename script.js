8c81edeb9a9113a2c5fe50e8f367436f69917bbe
frankie siino <frankie.siino@gmail.com>
2019-11-26 17:09:49 -0800

    Complete p5 project with simple drawing, color, and weight tool

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
-// P5 will actually create the canvas, not the HTML.
-// two functions:
-  // 1) setup - called once the app starts. defines the local env properties such as screen size and background color
-  // 2) draw called directly after setup(), continuously executes the code contained inside its block
-
-function setup() {
-  // create a canvas which is full width and height
-  createCanvas(window.innerWidth, window.innerHeight);
-
-  // add a white bg to t he canvas
-  background(255);
-}
-
-// const path = [];
-// Change approach to store all paths inside of paths array, instead of one path array of points. 
+const colorInput = document.getElementById('color');
+const weight = document.getElementById('weight');
+const clear = document.getElementById('clear');
 const paths = [];
 let currentPath = [];
 
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  background(255);
+}
+
 function draw() {
-  // disables filling geometry (p5-specific function)
   noFill();
 
   if (mouseIsPressed) {
-    // store the location of the mouse cursor
     const point = {
       x: mouseX,
       y: mouseY,
+      color: colorInput.value,
+      weight: weight.value
     };
-    paths.push(point);
+    currentPath.push(point);
   }
 
-  // loop over all paths and draw all points inside of them
-  beginShape();
-  paths.forEach(point => {
-    // create a vertex at the specified location
-    vertex(point.x, point.y);
+  paths.forEach(path => {
+    beginShape();
+    path.forEach(point => {
+      stroke(point.color);
+      strokeWeight(point.weight);
+      vertex(point.x, point.y);
+    });
+    endShape();
   });
-
-  endShape();
 }
 
-// When mouse is pressed, this fires (p5-specific function)
-function mousePressed() { 
-  // Clean up currentPath
+function mousePressed() {
   currentPath = [];
-  // Push the path inside the `paths` array
   paths.push(currentPath);
 }
+
+clear.addEventListener('click', () => {
+  paths.splice(0);
+  background(255);
+});
